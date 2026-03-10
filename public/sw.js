a006f3ee976f12d752dcb6bfc93a2b63464009f5
Вадик Пупкин <elips0675@gmail.com>
2026-03-10 04:21:44 +0000

    Если я скачаю зип сколько займет

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'swiftmatch-cache-v1';
+const CACHE_NAME = 'swiftmatch-v1';
 const ASSETS_TO_CACHE = [
   '/',
   '/manifest.json',
-  '/favicon.ico'
+  'https://fonts.googleapis.com/css2?family=Poppins:weight@800&family=Quicksand:wght@500;600;700&display=swap'
 ];
 
 self.addEventListener('install', (event) => {
